47e8bd8bf2a7622f6e11c095cb4acfdd002e68ac
rrollier1 <rrollier1@myges.fr>
2025-03-18 16:26:26 +0100

    26 bug Branch

--- deleteOldFile.ps1
+++ deleteOldFile.ps1
@@ -1,3 +1,3 @@
 $Path = Read-Host "Saisissez le chemin vers le dossier a purger!"
-#bug 25
+#bug 26
 Get-ChildItem -Path $path -Recurse -Force | Where-Object{ !$_.PSIsContainer -and $_. CreationTime -lt (Get-Date).AddDays(-15) } | Remove-Item -Force
