17ee55328ab329d26422228874e5735676bcfc96
rrollier1 <rrollier1@myges.fr>
2025-03-18 16:45:24 +0100

    Script 1

--- deleteOldFile.ps1
+++ deleteOldFile.ps1
@@ -1,3 +1,17 @@
-$Path = Read-Host "Saisissez le chemin vers le dossier a purger!"
-#bug 26
-Get-ChildItem -Path $path -Recurse -Force | Where-Object{ !$_.PSIsContainer -and $_. CreationTime -lt (Get-Date).AddDays(-15) } | Remove-Item -Force
+# Chemin du dossier temporaire
+$tempFolder = "C:\Path\To\Temp\Folder"  # Remplacez par le chemin de votre dossier temporaire
+
+# Fonction pour supprimer les fichiers temporaires
+function Supprimer-FichiersTemporaires {
+    param (
+        [string]$dossier
+    )
+
+    try {
+        # Supprimer tous les fichiers et dossiers dans le répertoire spécifié
+        Get-ChildItem -Path $dossier -Recurse | Remove-Item -Force -Recurse
+        Write-Output "Fichiers temporaires supprimés dans $dossier"
+    } catch {
+        Write-Output "Erreur lors de la suppression des fichiers : $_"
+    }
+}
