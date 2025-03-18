c14554fde29d69f66e90e10bac76bded5cf2ed4d
anthonin0909 <antho0909@live.fr>
2025-03-18 16:30:25 +0100

    bug27

--- deleteOldFile.ps1
+++ deleteOldFile.ps1
@@ -1,3 +1,3 @@
 $Path = Read-Host "Saisissez le chemin vers le dossier a purger!"
-#bug 25
+#bug 27
 Get-ChildItem -Path $path -Recurse -Force | Where-Object{ !$_.PSIsContainer -and $_. CreationTime -lt (Get-Date).AddDays(-15) } | Remove-Item -Force
